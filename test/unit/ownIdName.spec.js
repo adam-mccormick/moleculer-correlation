974ee9d4bbe21c262bd1b9b5a76760259df640f9
Matthias Endres <git@me23.de>
2023-07-04 22:00:15 +0200

    package updates  add logRelID to bindings and to log-msg. configure middleware

--- test/unit/ownIdName.spec.js
+++ test/unit/ownIdName.spec.js
@@ -2,11 +2,10 @@
 
 const { ServiceBroker } = require("moleculer");
 const { CorrelationMiddleware } = require("../../src");
-
 const { greeterService } = require("./greeterService");
 const { formaterService } = require("./formaterService");
 
-const logSpy = jest.fn().mockImplementation(() => false);
+const logSpy = jest.fn();
 let opts = { requestID: "" };
 
 const LogSpyMiddleware = {
@@ -16,7 +15,9 @@ const LogSpyMiddleware = {
 };
 
 describe("Test MyService", () => {
-	const broker = new ServiceBroker({ middlewares: [LogSpyMiddleware, CorrelationMiddleware] });
+	const broker = new ServiceBroker({
+		middlewares: [CorrelationMiddleware({ idName: "correlationID" }), LogSpyMiddleware]
+	});
 	const service = broker.createService(greeterService(opts));
 	broker.createService(formaterService);
 
@@ -24,7 +25,7 @@ describe("Test MyService", () => {
 	afterAll(() => broker.stop());
 
 	afterEach(() => {
-		opts.requestID = "";
+		opts.requestID = undefined;
 		logSpy.mockReset();
 	});
 
@@ -32,23 +33,7 @@ describe("Test MyService", () => {
 		expect(service).toBeDefined();
 	});
 
-	it("should add emptyContextStr if not in Context", async () => {
-		service.logger.info("def");
-		expect(logSpy.mock.calls[0]).toMatchObject([
-			"info",
-			["[No Context] ", "def"],
-			{
-				nodeID: expect.any(String),
-				ns: "",
-				mod: "greeter",
-				svc: "greeter",
-				ver: undefined,
-				logRelID: "No Context"
-			}
-		]);
-	});
-
-	it("should return with 'Hello Anonymous' and add requestID to log statements", () => {
+	it("should return with 'Hello Anonymous' and add opts.requestID to log statements", () => {
 		return broker.call("greeter.greet").then(res => {
 			expect(res).toBe("Hello Anonymous!");
 			expect(logSpy.mock.calls[0]).toMatchObject([
@@ -60,13 +45,13 @@ describe("Test MyService", () => {
 					mod: "greeter",
 					svc: "greeter",
 					ver: undefined,
-					logRelID: opts.requestID
+					correlationID: opts.requestID
 				}
 			]);
 		});
 	});
 
-	it("should return with 'Hello John' and add same requestID to all log statements ", () => {
+	it("should return with 'Hello John' and add same opts.requestID to all log statements ", () => {
 		return broker.call("greeter.greet", { name: "John" }).then(res => {
 			expect(res).toBe("Hello John!");
 			expect(logSpy.mock.calls).toMatchObject([
@@ -79,7 +64,7 @@ describe("Test MyService", () => {
 						mod: "greeter",
 						svc: "greeter",
 						ver: undefined,
-						logRelID: opts.requestID
+						correlationID: opts.requestID
 					}
 				],
 				[
@@ -91,7 +76,7 @@ describe("Test MyService", () => {
 						mod: "formatter",
 						svc: "formatter",
 						ver: undefined,
-						logRelID: opts.requestID
+						correlationID: opts.requestID
 					}
 				],
 				[
@@ -103,30 +88,10 @@ describe("Test MyService", () => {
 						mod: "formatter",
 						svc: "formatter",
 						ver: undefined,
-						logRelID: opts.requestID
+						correlationID: opts.requestID
 					}
 				]
 			]);
 		});
-	});
-
-	it("should use ParentCtx to get logRelID", () => {
-		return broker
-			.call("greeter.greet", {}, { parentCtx: { meta: { logRelID: "abc" } } })
-			.then(res => {
-				expect(res).toBe("Hello Anonymous!");
-				expect(logSpy.mock.calls[0]).toMatchObject([
-					"info",
-					["[abc] ", "Creating a greeting for undefined"],
-					{
-						nodeID: expect.any(String),
-						ns: "",
-						mod: "greeter",
-						svc: "greeter",
-						ver: undefined,
-						logRelID: "abc"
-					}
-				]);
-			});
 	});
 });
--- test/unit/supress_in_logMsg.spec.js
+++ test/unit/supress_in_logMsg.spec.js
@@ -2,11 +2,10 @@
 
 const { ServiceBroker } = require("moleculer");
 const { CorrelationMiddleware } = require("../../src");
-
 const { greeterService } = require("./greeterService");
 const { formaterService } = require("./formaterService");
 
-const logSpy = jest.fn().mockImplementation(() => false);
+const logSpy = jest.fn();
 let opts = { requestID: "" };
 
 const LogSpyMiddleware = {
@@ -16,7 +15,9 @@ const LogSpyMiddleware = {
 };
 
 describe("Test MyService", () => {
-	const broker = new ServiceBroker({ middlewares: [LogSpyMiddleware, CorrelationMiddleware] });
+	const broker = new ServiceBroker({
+		middlewares: [CorrelationMiddleware({ addToLogMsg: false }), LogSpyMiddleware]
+	});
 	const service = broker.createService(greeterService(opts));
 	broker.createService(formaterService);
 
@@ -24,7 +25,7 @@ describe("Test MyService", () => {
 	afterAll(() => broker.stop());
 
 	afterEach(() => {
-		opts.requestID = "";
+		opts.requestID = undefined;
 		logSpy.mockReset();
 	});
 
@@ -32,28 +33,12 @@ describe("Test MyService", () => {
 		expect(service).toBeDefined();
 	});
 
-	it("should add emptyContextStr if not in Context", async () => {
-		service.logger.info("def");
-		expect(logSpy.mock.calls[0]).toMatchObject([
-			"info",
-			["[No Context] ", "def"],
-			{
-				nodeID: expect.any(String),
-				ns: "",
-				mod: "greeter",
-				svc: "greeter",
-				ver: undefined,
-				logRelID: "No Context"
-			}
-		]);
-	});
-
-	it("should return with 'Hello Anonymous' and add requestID to log statements", () => {
+	it("should return with 'Hello Anonymous' and add opts.requestID to log statements", () => {
 		return broker.call("greeter.greet").then(res => {
 			expect(res).toBe("Hello Anonymous!");
 			expect(logSpy.mock.calls[0]).toMatchObject([
 				"info",
-				["[" + opts.requestID + "] ", "Creating a greeting for undefined"],
+				["Creating a greeting for undefined"],
 				{
 					nodeID: expect.any(String),
 					ns: "",
@@ -66,13 +51,13 @@ describe("Test MyService", () => {
 		});
 	});
 
-	it("should return with 'Hello John' and add same requestID to all log statements ", () => {
+	it("should return with 'Hello John' and add same opts.requestID to all log statements ", () => {
 		return broker.call("greeter.greet", { name: "John" }).then(res => {
 			expect(res).toBe("Hello John!");
 			expect(logSpy.mock.calls).toMatchObject([
 				[
 					"info",
-					["[" + opts.requestID + "] ", "Creating a greeting for John"],
+					["Creating a greeting for John"],
 					{
 						nodeID: expect.any(String),
 						ns: "",
@@ -84,7 +69,7 @@ describe("Test MyService", () => {
 				],
 				[
 					"info",
-					["[" + opts.requestID + "] ", "Formatting name John"],
+					["Formatting name John"],
 					{
 						nodeID: expect.any(String),
 						ns: "",
@@ -96,7 +81,7 @@ describe("Test MyService", () => {
 				],
 				[
 					"info",
-					["[" + opts.requestID + "] ", "Request id in method for name John"],
+					["Request id in method for name John"],
 					{
 						nodeID: expect.any(String),
 						ns: "",
@@ -108,25 +93,5 @@ describe("Test MyService", () => {
 				]
 			]);
 		});
-	});
-
-	it("should use ParentCtx to get logRelID", () => {
-		return broker
-			.call("greeter.greet", {}, { parentCtx: { meta: { logRelID: "abc" } } })
-			.then(res => {
-				expect(res).toBe("Hello Anonymous!");
-				expect(logSpy.mock.calls[0]).toMatchObject([
-					"info",
-					["[abc] ", "Creating a greeting for undefined"],
-					{
-						nodeID: expect.any(String),
-						ns: "",
-						mod: "greeter",
-						svc: "greeter",
-						ver: undefined,
-						logRelID: "abc"
-					}
-				]);
-			});
 	});
 });
